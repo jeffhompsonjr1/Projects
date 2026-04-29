03a18ecef5b42007cceaab1097c219259a0f1e22
jeffthompsonjr1 <jeff.thompsonjr1@gmail.com>
2026-04-29 18:58:12 -0500

    Create Performance_Script.sh

---
+++ Performance_Script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This is a script to gather and analyze daily performance of my linux systems.
+TIMESTAMP=$(date +%c)
+FILE="/home/sysadmin/Performance_Monitoring/PM_from_$TIMESTAMP.txt"
+
+#Header of document
+echo -e "\t Performance Monitor Log - $TIMESTAMP" >> "$FILE"
+echo "" >> "$FILE"
+
+#Appended Commands
+echo -e "\n\t ================== JOURNAL ERRORS LAST 1 HOUR ======================\n" >> "$FILE"
+journalctl -p err --since "1 hour ago" >> "$FILE"
+
+echo -e "\n\t ==================== KERNEL ERRORS LAST 1 HOUR ======================\n" >> "$FILE"
+journalctl -k -p err --since "1 hour ago" >> "$FILE"
+
+echo -e "\n\t =================== SSH AUTHENTICATION WARNINGS=====================\n" >> "$FILE"
+journalctl -u sshd -p warning --since "1 hour ago" >> "$FILE"
+
+echo -e "\n\t===== FAILED SYSTEMD SERVICES =====" >> "$FILE"
+systemctl --failed >> "$FILE"
+
+echo -e "\n\t ================== DISK SPACE ======================\n" >> "$FILE"
+df -h >> "$FILE"
+
+echo -e "\n\t ================== MEMORY SPACE ======================\n" >> "$FILE"
+free -h >> "$FILE"
+
+echo -e "\n\t ================== VOLUME ALLOCATION ======================\n" >> "$FILE"
+lsblk /dev/sd* >> "$FILE"
+
+echo -e "\n\t ================== TOP OUTPUT======================\n" >> "$FILE"
+top -b -n 1 >> "$FILE"
