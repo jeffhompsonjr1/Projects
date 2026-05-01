733542fe85fe460e43ad23682fbf359528562944
jeffthompsonjr1 <jeff.thompsonjr1@gmail.com>
2026-04-30 20:49:59 -0500

    Python Practice for 30 April

--- Python Practice/9-1 Resturant.py
+++ Python Practice/9-1 Resturant.py
@@ -5,6 +5,7 @@ class Resturant:
         """ Initialize name and cuisine attributes"""
         self.name = name 
         self.cuisine = cuisine
+        self.number_served = 0
     
     def describe_resturant(self):
         """Print a summary of the resturant"""
@@ -12,9 +13,25 @@ class Resturant:
     
     def open_resturant(self):
         """Print a message indicating that the resturant is open"""
-        print(f"{self.name} is now open!")  
+        print(f"{self.name} is now open!") 
 
-my_resturant = Resturant('The Great Wall', 'Chinese')
+    def set_number_served(self, number):
+        """Set the number_served"""
+        self.number_served = number
+        print(f"We served {self.number_served} today.")
 
+    def increment_number_served(self, number_added):
+        """Increement the number of customers who've been served"""
+        self.number_served += number_added
+        print(f"We've served {self.number_served} today.")
+
+
+my_resturant = Resturant("Lovely Creations", "Desserts")
 my_resturant.describe_resturant()
-my_resturant.open_resturant()
+my_resturant.increment_number_served(10)
+my_resturant.increment_number_served(15)
+
+            
+
+
+
